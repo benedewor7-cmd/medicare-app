4e9f4d7de3611c31ef7460ec70f355cfbda8737c
benedewor7-cmd <benedewor7@gmail.com>
2026-09-11 17:36:43 +0100

    Fix redirection and form action in login.php

--- login.php
+++ login.php
@@ -276,7 +276,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                             */
 
                             redirect(
-                                'dashboard'
+                                'dashboard.php'
                             );
 
                             exit;
@@ -2458,7 +2458,7 @@ $csrfToken =
 
             <form
                 method="POST"
-                action=""
+                action="login.php"
                 autocomplete="on"
             >
 
